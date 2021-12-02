63e5481e2ee7f520ac730eb7e87e434e055cdf10
Iskander Sharipov <quasilyte@gmail.com>
2021-12-03 00:14:53 +0300

    show total SLOC count in the info section

--- app.js
+++ app.js
@@ -1,81 +1,45 @@
-namespace pako {
-    export declare function ungzip(r);
-}
-
-declare function untar(tar);
-
-namespace App {
-    declare class Go {
-    }
-
-    interface loadRepoResult {
-        repo: repositoryInfo;
-        archive: Uint8Array;
-    }
-
-    interface gogrepResult {
-        error?: string;
-        matches?: string[];
-    }
-
-    interface corpusInfo {
-        Repositories: repositoryInfo[];
-    }
-
-    interface repositoryInfo {
-        Name: string;
-        Tags: string[];
-        Git: string;
-        Commit: string;
-        Size: number;
-        MinifiedSize: number;
-        SLOC: number;
-        Files: repositoryFileInfo[];
-    }
-
-    interface repositoryFileInfo {
-        Name: string;
-        Flags: number;   
-    }
-
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var pako;
+(function (pako) {
+})(pako || (pako = {}));
+var App;
+(function (App) {
     class RepoData {
-        files: RepoFileData[] = [];
+        constructor() {
+            this.files = [];
+        }
     }
-
     class RepoFileData {
-        name: string;
-        contents: string;
     }
-
-    declare function gogrep(pattern: string, filename: string, src: string): gogrepResult;
-
     const appState = {
-        metadata: <corpusInfo>(null),
-        corpus: new Map<string, RepoData>(),
-
+        metadata: (null),
+        corpus: new Map(),
         go: null,
         wasm: null,
-
         busy: false,
-
         // Current query state.
-        searchResults: new Map<string, number>(),
+        searchResults: new Map(),
         filesTotal: 0,
         filesScanned: 0,
         hits: 0,
     };
-
-    function updateStatus(status: string) {
+    function updateStatus(status) {
         document.getElementById('status').innerText = status;
     }
-
     function loadMetadata() {
         updateStatus('loading metadata');
         console.log("loadMetadata()");
         return fetch('corpus-output/corpus.json')
             .then(response => response.json());
     }
-
     function loadWASM(metadata) {
         appState.metadata = metadata;
         let $totalSLOC = document.getElementById('total-sloc');
@@ -84,7 +48,6 @@ namespace App {
             totalSLOC += repo.SLOC;
         }
         $totalSLOC.innerText = totalSLOC.toLocaleString();
-
         updateStatus('loading wasm');
         console.log("loadWASM()");
         appState.go = new Go();
@@ -94,7 +57,6 @@ namespace App {
                 .catch(error => reject(error));
         });
     }
-
     function doChunks(count, f, finished) {
         var i = 0;
         (function step() {
@@ -102,13 +64,13 @@ namespace App {
             i++;
             if (i < count) {
                 setTimeout(step, 0);
-            } else {
+            }
+            else {
                 finished();
             }
         })();
     }
-
-    function runQueryRecursive(pattern: string, toScan: repositoryInfo[]) {
+    function runQueryRecursive(pattern, toScan) {
         if (toScan.length == 0) {
             appState.busy = false;
             updateStatus('ready');
@@ -125,53 +87,49 @@ namespace App {
             $results.innerHTML += '<ol>' + parts.join('') + '</ol>';
             return;
         }
-
         let repo = toScan.pop();
         let repoData = appState.corpus.get(repo.Name);
-        let files = repoData.files; 
-        doChunks(files.length,
-            i => {
-                let f = files[i];
-                updateStatus(`processing ${f.name}`);
-                let $progress = document.getElementById('search-progress');
-                let progressValue = Math.round((appState.filesScanned / appState.filesTotal) * 100);
-                $progress.innerHTML = `Progress: ${progressValue}% (hits: ${appState.hits})`;
-                let result = gogrep(pattern, f.name, f.contents);
-                if (result.error) {
-                    console.error(`grepping ${f.name}: ${result.error}`);
-                    return;
+        let files = repoData.files;
+        doChunks(files.length, i => {
+            let f = files[i];
+            updateStatus(`processing ${f.name}`);
+            let $progress = document.getElementById('search-progress');
+            let progressValue = Math.round((appState.filesScanned / appState.filesTotal) * 100);
+            $progress.innerHTML = `Progress: ${progressValue}% (hits: ${appState.hits})`;
+            let result = gogrep(pattern, f.name, f.contents);
+            if (result.error) {
+                console.error(`grepping ${f.name}: ${result.error}`);
+                return;
+            }
+            appState.filesScanned++;
+            if (!result.matches) {
+                return;
+            }
+            appState.hits += result.matches.length;
+            for (let m of result.matches) {
+                if (appState.searchResults.size >= 1000) {
+                    break;
                 }
-                appState.filesScanned++;
-                if (!result.matches) {
-                    return;
+                if (appState.searchResults.has(m)) {
+                    appState.searchResults.set(m, appState.searchResults.get(m) + 1);
                 }
-                appState.hits += result.matches.length;
-                for (let m of result.matches) {
-                    if (appState.searchResults.size >= 1000) {
-                        break;
-                    }
-                    if (appState.searchResults.has(m)) {
-                        appState.searchResults.set(m, appState.searchResults.get(m) + 1);
-                    } else {
-                        appState.searchResults.set(m, 1);
-                    }
+                else {
+                    appState.searchResults.set(m, 1);
                 }
-            },
-            () => {
-                runQueryRecursive(pattern, toScan);
-            });
+            }
+        }, () => {
+            runQueryRecursive(pattern, toScan);
+        });
     }
-
-    function loadRepo(repo: repositoryInfo) {
+    function loadRepo(repo) {
         updateStatus(`loading ${repo.Name} repository...`);
         return fetch(`corpus-output/${repo.Name}.tar.gz`).
-            then(result => new Promise<loadRepoResult>((resolve, reject) => {
-                result.arrayBuffer().
-                    then(b => resolve({repo: repo, archive: new Uint8Array(b)}))
-            }));
+            then(result => new Promise((resolve, reject) => {
+            result.arrayBuffer().
+                then(b => resolve({ repo: repo, archive: new Uint8Array(b) }));
+        }));
     }
-
-    function loadRecursive(toLoad: repositoryInfo[]) {
+    function loadRecursive(toLoad) {
         if (toLoad.length == 0) {
             appState.busy = false;
             updateStatus('ready');
@@ -183,43 +141,38 @@ namespace App {
             .then(arr => arr.buffer)
             .then(buf => untar(buf))
             .then(files => {
-                var repoData = new RepoData();
-                var dec = new TextDecoder("utf-8");
-                for (let rawFile of files) {
-                    let f = new RepoFileData();
-                    f.name = rawFile.name;
-                    f.contents = dec.decode(rawFile.buffer);
-                    repoData.files.push(f);
-                }
-                console.log(`loaded ${repo.Name} repo`);
-                appState.corpus.set(repo.Name, repoData);
-
-                let $checkbox = <HTMLInputElement>(document.getElementById(`repository-${repo.Name}`));
-                $checkbox.parentElement.classList.add('blue-text');
-
-                loadRecursive(toLoad);
-            });
+            var repoData = new RepoData();
+            var dec = new TextDecoder("utf-8");
+            for (let rawFile of files) {
+                let f = new RepoFileData();
+                f.name = rawFile.name;
+                f.contents = dec.decode(rawFile.buffer);
+                repoData.files.push(f);
+            }
+            console.log(`loaded ${repo.Name} repo`);
+            appState.corpus.set(repo.Name, repoData);
+            let $checkbox = (document.getElementById(`repository-${repo.Name}`));
+            $checkbox.parentElement.classList.add('blue-text');
+            loadRecursive(toLoad);
+        });
     }
-
-    function getSelectedRepos(): repositoryInfo[] {
+    function getSelectedRepos() {
         var selected = [];
         for (let i = 0; i < appState.metadata.Repositories.length; i++) {
             let repo = appState.metadata.Repositories[i];
-            let $checkbox = <HTMLInputElement>(document.getElementById(`repository-${repo.Name}`));
+            let $checkbox = (document.getElementById(`repository-${repo.Name}`));
             if ($checkbox.checked) {
                 selected.push(repo);
             }
         }
         return selected;
     }
-
     function loadRepositories() {
         if (appState.busy) {
             return;
         }
-
         appState.busy = true;
-        let toLoad: repositoryInfo[] = [];
+        let toLoad = [];
         for (let repo of getSelectedRepos()) {
             if (!appState.corpus.has(repo.Name)) {
                 toLoad.push(repo);
@@ -227,7 +180,6 @@ namespace App {
         }
         loadRecursive(toLoad);
     }
-
     function setupUI() {
         var $corpusSelection = document.getElementById('corpus-selection');
         var selectionHTML = '';
@@ -249,21 +201,19 @@ namespace App {
         };
         buildSelectionTable();
         $corpusSelection.innerHTML = '<table style="width: 100%; display: table">' + selectionHTML + '</table>';
-
         let $loadRepos = document.getElementById('load-button');
-        $loadRepos.onclick = function() {
+        $loadRepos.onclick = function () {
             loadRepositories();
         };
-
         var $run = document.getElementById('run-button');
         var $results = document.getElementById('search-results');
         $results.innerHTML = '';
-        $run.onclick = function() {
+        $run.onclick = function () {
             if (appState.busy) {
                 return;
             }
             appState.busy = true;
-            let pattern = (<HTMLTextAreaElement>document.getElementById('search-pattern')).value;
+            let pattern = document.getElementById('search-pattern').value;
             $results.innerHTML = '';
             appState.searchResults.clear();
             appState.filesScanned = 0;
@@ -278,7 +228,6 @@ namespace App {
             runQueryRecursive(pattern, repos);
         };
     }
-
     function runApp(wasm) {
         updateStatus('ready');
         console.log("runApp()");
@@ -286,24 +235,22 @@ namespace App {
         appState.go.run(wasm);
         setupUI();
     }
-
     function initPolyfills() {
-        if (!WebAssembly.instantiateStreaming) { 
-            WebAssembly.instantiateStreaming = async (resp, importObject) => {
-                const r = await (await resp).arrayBuffer();
-                return await WebAssembly.instantiate(r, importObject);
-            }
+        if (!WebAssembly.instantiateStreaming) {
+            WebAssembly.instantiateStreaming = (resp, importObject) => __awaiter(this, void 0, void 0, function* () {
+                const r = yield (yield resp).arrayBuffer();
+                return yield WebAssembly.instantiate(r, importObject);
+            });
         }
     }
-
-    export function main() {
+    function main() {
         initPolyfills();
         loadMetadata().
             then(metadata => loadWASM(metadata)).
             then(wasm => runApp(wasm));
     }
-}
-
-window.onload = function() { 
+    App.main = main;
+})(App || (App = {}));
+window.onload = function () {
     App.main();
 };
